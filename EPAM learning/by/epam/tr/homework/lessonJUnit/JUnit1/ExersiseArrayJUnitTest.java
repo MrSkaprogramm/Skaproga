6a62ad3cbdd5068882ecdd6da39460451d0697b8
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-10-11 00:36:12 +0300

    Update ExersiseArrayJUnitTest.java

--- EPAM learning/by/epam/tr/homework/lessonJUnit/JUnit1/ExersiseArrayJUnitTest.java
+++ EPAM learning/by/epam/tr/homework/lessonJUnit/JUnit1/ExersiseArrayJUnitTest.java
@@ -1,10 +1,15 @@
 package by.epam.tr.main;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ExersiseArrayJUnitTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+				@Test
+	public void ZeroTest() {
+				int[] mas = {1,2,3,4,5,6,7,8,9,10,11};
+				int[] realmas;
+				int[] expectedmas = {1,0,3,0,4,0,6,0,8,0,10,0};
+				realmas = ExersiseArrayJUnit.newArray(mas);
+				Assert.assertEquals(expectedmas, realmas);
+				}
 	}
-
-}
