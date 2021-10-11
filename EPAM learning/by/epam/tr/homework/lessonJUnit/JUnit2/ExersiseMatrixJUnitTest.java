7c97979d74835fc84c7523b58ea3209998b45d25
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-10-11 05:23:39 +0300

    Update ExersiseMatrixJUnitTest.java

--- EPAM learning/by/epam/tr/homework/lessonJUnit/JUnit2/ExersiseMatrixJUnitTest.java
+++ EPAM learning/by/epam/tr/homework/lessonJUnit/JUnit2/ExersiseMatrixJUnitTest.java
@@ -1,10 +1,15 @@
-package by.epam.tr.main;
-
 public class ExersiseMatrixJUnitTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+				@Test
+	public void ZeroTest() {
+				int[] mas = {2,2,3};
+				int[][] realmas;
+				int[][] expectedmas = {{1, 1, 1}, {2, 2, 3}, {4, 4, 9}};
+				int n = 3;
+				realmas = ExersiseMatrixJUnit.Printmatrix(n, mas);
+				for(int i = 0; i < expectedmas.length; i++){
+					for(int j = 0; j < realmas[j].length - 1; j++){
+						Assert.assertEquals(expectedmas[i][j], realmas[i][j]);
+			}
+		}
 	}
-
 }
